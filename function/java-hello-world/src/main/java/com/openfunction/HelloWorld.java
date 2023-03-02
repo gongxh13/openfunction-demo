c47ba82336b3a8463d482ec4bed36dd47550a53c
gongxh <gongxh0424@gmail.com>
2023-03-03 00:36:03 +0800

    change java hello world bug

--- function/java-hello-world/src/main/java/com/openfunction/HelloWorld.java
+++ function/java-hello-world/src/main/java/com/openfunction/HelloWorld.java
@@ -1,17 +1,15 @@
 package com.openfunction;
 
-import com.google.cloud.functions.HttpFunction;
-import com.google.cloud.functions.HttpRequest;
-import com.google.cloud.functions.HttpResponse;
+import dev.openfunction.functions.HttpFunction;
+import dev.openfunction.functions.HttpRequest;
+import dev.openfunction.functions.HttpResponse;
 import java.io.BufferedWriter;
 import java.io.IOException;
 
 public class HelloWorld implements HttpFunction {
-  // Simple function to return "Hello World"
-  @Override
-  public void service(HttpRequest request, HttpResponse response)
-      throws IOException {
-    BufferedWriter writer = response.getWriter();
-    writer.write("Hello World!");
-  }
+    @Override
+    public void service(HttpRequest request, HttpResponse response) throws Exception {
+
+        response.getWriter().write("Hello World");
+    }
 }

af0cdf5f296a995425e26d17660131dd2b3ad95a
gongxh <gongxh0424@gmail.com>
2023-03-20 10:25:47 +0800

    add lock demo

--- dapr/go/lock/code/main.go
+++ dapr/go/lock/code/main.go
@@ -35,7 +35,7 @@ func main() {
 		ResourceID: "my_file_name",
 	})
 
-    fmt.Println("Release lock result:%s, error is %s", resp1.Status)
+    fmt.Println("Release lock result:%s, error is %s", resp1.Status, err1)
 
     resp, err = client.TryLockAlpha1(ctx, "lockstore", &dapr.LockRequest{
 		LockOwner:         "random_id_abc123",

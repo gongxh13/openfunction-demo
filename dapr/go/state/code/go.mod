c440b421c768dbd1fc6f37ff6b3a969223c4edbc
gongxh <gongxh0424@gmail.com>
2023-03-20 15:59:10 +0800

    add state demo

---
+++ dapr/go/state/code/go.mod
@@ -0,0 +1,7 @@
+module example.com/subscriber
+
+go 1.17
+
+require (
+	github.com/dapr/go-sdk v1.7.0
+)
---
+++ dapr/go/state/code/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"context"
+	"log"
+	"math/rand"
+	"strconv"
+	"time"
+
+	dapr "github.com/dapr/go-sdk/client"
+)
+
+func main() {
+	const STATE_STORE_NAME = "statestore"
+	rand.Seed(time.Now().UnixMicro())
+	ctx := context.Background()
+    client, err := dapr.NewClient()
+    if err != nil {
+        panic(err)
+    }
+    defer client.Close()
+
+	err = client.SaveState(ctx, STATE_STORE_NAME, "order_1", []byte(strconv.Itoa(orderId)), nil)
+	if err != nil {
+		panic(err)
+	}
+	result, err := client.GetState(ctx, STATE_STORE_NAME, "order_1", nil)
+	if err != nil {
+		panic(err)
+	}
+	log.Println("Result after get:", string(result.Value))
+
+	if err = client.DeleteState(ctx, STATE_STORE_NAME, "order_1"); err != nil {
+        panic(err)
+    }
+}

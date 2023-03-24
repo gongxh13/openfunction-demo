36d68f36c839e0570fb4bf5c6db289ff8a483249
gongxh13 <44743148+gongxh13@users.noreply.github.com>
2023-03-24 10:48:19 +0800

    Update from.go

--- function-dapr/invoke/from/from.go
+++ function-dapr/invoke/from/from.go
@@ -12,7 +12,7 @@ import (
 func FromFunc(ctx ofctx.Context, in []byte) (ofctx.Out, error) {
 	log.Printf("event - Data: %s", in)
 	ctxb := context.Background()
-	client, err := dapr.NewClientWithAddress("")
+	client, err := dapr.NewClientWithAddress("function-dapr-invoke-from-default-dapr.default.svc.cluster.local:50001")
 	if err != nil {
 		panic(err)
 	}
